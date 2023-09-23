18a1a895c3e8116e7348ebf411682b81ee161360
Pyshrub <thepyromancer2048@gmail.com>
2023-09-23 18:32:33 -0400

    bugfixes
    Made a mistake somewhere, so fixed the bugs in the literal main menu

--- Scene/T_Screen/control.gd
+++ Scene/T_Screen/control.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var clouds = $Skybox
+@onready var clouds = $'Skybox'
 @onready var start = $title_screen/Label/startime
 func _ready():
 	clouds.play("title")
@@ -15,7 +15,3 @@ func _on_button_pressed():
 	get_node("title_screen").hide()
 	start.paused = true
 	get_node("m_menu").show()
-
-
-	
-	
